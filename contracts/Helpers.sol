a6dfb4b5322dafeb2c65241fcf0546f51b7a1b8e
blah <tim.galebach@gmail.com>
2017-10-07 14:56:00 +0000

    helpers library added

---
+++ contracts/Helpers.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.15;
+
+library Helpers {
+  function compare(bytes32 a, bytes32 b) private constant returns (int) {
+    //    bytes memory a = bytes(_a);
+    //    bytes memory b = bytes(_b);
+    uint minLength = a.length;
+    if (b.length < minLength) minLength = b.length;
+    for (uint i = 0; i < minLength; i ++)
+      if (a[i] < b[i])
+        return -1;
+      else if (a[i] > b[i])
+        return 1;
+    if (a.length < b.length)
+      return -1;
+    else if (a.length > b.length)
+      return 1;
+    else
+      return 0;
+  }
+}
